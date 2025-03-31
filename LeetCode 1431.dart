929f2338b96897db923bedc04232df605f71f4e7
KKK <kywmania0908@gmail.com>
2025-03-31 09:48:21 +0900

    2025 03 31

--- LeetCode 1431.dart
+++ LeetCode 1431.dart
@@ -1,12 +1,12 @@
 class Solution {
   List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
     // 최대값 구하기
-    int maxCandie = candies.reduce((a, b) => a > b ? a : b);
+    int maxCandy = candies.reduce((a, b) => a > b ? a : b);
     // 결과값 담을 리스트 초기화
     List<bool> result = [];
 
     for (int candie in candies) {
-      candie + extraCandies >= maxCandie ? result.add(true) : result.add(false);
+      candie + extraCandies >= maxCandy ? result.add(true) : result.add(false);
     }
 
     return result;
